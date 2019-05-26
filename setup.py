8a2c639fca46c7b198481519a6512ad1821e971b
tobi08151405 <tobias.teichmann@gmx.at>
2019-05-26 16:23:23 +0200

    changed version of setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="teichicrypt",
-    version="1.1.0",
+    version="1.1.1",
     author="Teichi",
     author_email="tobias@teichmann.top",
     description="self build crypting algorithm",
